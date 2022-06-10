4cbdd4e3412d6c9a5f0cab38b098256d5b84316f
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-06-10 10:44:57 -0300

    Sacando todo lo anterior

--- Editor/Ingredientes/ICambiante.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ItIsNotOnlyMe.SistemaDePociones
-{
-    public interface ICambiante
-    {
-        public void AgregarModificador(ICambiar modificador);
-
-        public void SacarModificador(ICambiar modificador);
-    }
-}
